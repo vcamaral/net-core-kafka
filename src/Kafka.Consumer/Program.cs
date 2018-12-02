2f019d4ee06bb0cfb2691f9f88caaed5d211cada
Vinicius Amaral <vcamaral@gmail.com>
2018-12-02 14:48:26 -0200

    Finaliza consumidor.

--- src/Kafka.Consumer/Program.cs
+++ src/Kafka.Consumer/Program.cs
@@ -1,9 +1,46 @@
-﻿namespace Kafka.Consumer
+﻿using Confluent.Kafka;
+using System;
+
+namespace Kafka.Consumer
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                Console.WriteLine($"Usage: Kafka.Consumer <topic>");
+                return;
+            }
+
+            var topic = args[0];
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = "test-consumer",
+                AutoOffsetReset = AutoOffsetResetType.Earliest
+            };
+
+            using (var consumer = new Consumer<Null, string>(config))
+            {
+                consumer.Subscribe(topic);
+
+                Console.WriteLine($"Consuming topic {topic}...");
+
+                while (true)
+                {
+                    try
+                    {
+                        var result = consumer.Consume();
+                        Console.WriteLine($"Message {result.Value} consumed");
+                    }
+                    catch (ConsumeException ex)
+                    {
+                        Console.WriteLine($"Error to consume message: {ex.Error.Reason}");
+                    }
+                }
+            }
         }
     }
 }
